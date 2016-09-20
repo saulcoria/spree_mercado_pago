938e68bc2726d80e0889354f3bb0fd8f708b2bc9
Martín Acevedo <macevedo@web-experto.com.ar>
2016-09-20 16:27:26 -0300

    Se implementó el botón de Mercado Pago
    Se modificó la implementación de Mercado Pago, para que en lugar de un
    link, la extensión bindea al botón de confirmar la compra la función
    necesaria para que el sistema redirija a Mercado Pago, sólo si este
    último es elegido como medio de pago.

--- app/assets/javascripts/spree/frontend/spree_mercado_pago.js
+++ app/assets/javascripts/spree/frontend/spree_mercado_pago.js
@@ -1,5 +1,4 @@
 //= require spree/frontend
-
 MercadoPago = {
   hidePaymentSaveAndContinueButton: function(paymentMethod) {
     if (MercadoPago.paymentMethodID && paymentMethod.val() == MercadoPago.paymentMethodID) {
@@ -12,13 +11,26 @@ MercadoPago = {
   }
 };
 
+function bindMercadoPagoPaymentButton(){
+    $('#go_to_confirm').attr('type','submit');
+
+    $("#payment-method-fields li").each(
+        function(){
+            if(($(this).find('label').text().match(/^\s+Mercado Pago\s+$/)) && ($(this).find('input').is(":checked"))){
+                $('#go_to_confirm').bind('click',function(){});
+                $('#go_to_confirm').attr('type','button');
+                $('#go_to_confirm').bind('click',$('#btnMercadoPago').click());
+            }
+        }
+    ); 
+}
+
 jQuery(document).ready(function() {
   checkedPaymentMethod = jQuery('#order_payments_attributes__payment_method_id');
   MercadoPago.hidePaymentSaveAndContinueButton(checkedPaymentMethod);
   paymentMethods = jQuery('#order_payments_attributes__payment_method_id').click(function (e) {
     MercadoPago.hidePaymentSaveAndContinueButton($(e.target));
   });
-
   jQuery('button.mercado_pago_button').click(function(event){
     jQuery(event.target).prop("disabled",true);
   });
